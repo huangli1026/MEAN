05bcd1a2bae33fb81cc4c1de891aedeccbc568c6
lihuang1026 <lihuang1026@gmail.com>
2024-04-01 16:24:04 -0400

    reduce update, solve optional initial value issue

--- Forth_Homework/filter_reduce_prototype.js
+++ Forth_Homework/filter_reduce_prototype.js
@@ -11,12 +11,13 @@ Array.prototype.myFilter=function(Fn){
 let arr=[1,2,3,4,5];
 console.log(arr.myFilter((x)=>x%2===0)) //output [2,4]
 
-Array.prototype.myReduce=function(Fn){
-    let result=0;
+Array.prototype.myReduce=function(Fn, init){
+    let result=init?init:0;
     for(let i=0; i<this.length; i++){
         result=Fn(result, this[i], i)
     }
-    return result
+    return result;
 }
 
-console.log(arr.myReduce((x ,y)=>x+y))
+console.log(arr.myReduce((x ,y)=>x+y)) 
+console.log(arr.myReduce(((x,y)=>x+y), 10))
